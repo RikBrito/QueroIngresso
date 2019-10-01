db4d6d1dfc14855d004ccc50c1122da8fd835537
kerherick <50116988+kerherick@users.noreply.github.com>
2019-09-30 23:28:00 -0300

    slider and Fab

--- src/pages/explorer/explorer.js
+++ src/pages/explorer/explorer.js
@@ -1,24 +1,58 @@
-import explorar from './assets/explorar.png';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Slider from "@material-ui/core/Slider";
+import Fab from "@material-ui/core/Fab";
+import NavigationIcon from "@material-ui/icons/Navigation";
 
-import { StyleSheet, Image, View, TextInput} from 'react-native';
+ const useStyles = makeStyles(theme => ({
+  root: {
+    width: 300
+  },
+  margin: {
+    height: theme.spacing(3)
+  },
+   
+  extendedIcon: {
+    marginRight: theme.spacing(1)
+  }
+}));
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-
-      <Image source ={explorar}/>
-    </View>
-
-
-    
-    
-  );
+function valuetext(value) {
+  return `${value}°C`;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-        
-  },
-});
+function valueLabelFormat(value) {
+  return marks.findIndex(mark => mark.value === value) + 1;
+}
+
+export default function DiscreteSlider() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Slider
+        defaultValue={30}
+        getAriaValueText={valuetext}
+        aria-labelledby="discrete-slider"
+        valueLabelDisplay="auto"
+        step={10}
+        marks
+        min={10}
+        max={110}
+      />
+      <div className={classes.margin} />
+
+      <div>
+      <Fab
+        variant="extended"
+        size="small"
+        color="primary"
+        aria-label="add"
+        className={classes.margin}
+      >
+        bares
+      </Fab>
+    </div>
+    </div>
+  );
+}
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,9 @@
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 import Home from '../pages/Home';
-
-
+import Demo from '../../App';
+import ReactDOM from 'react-dom';
+ReactDOM.render(<Demo />, document.querySelector('#root'));
+ 
 const HomeRoutes = createStackNavigator(
   {
     Home: { screen: Home },
